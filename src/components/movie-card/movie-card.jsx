772238a4a2d997c8a5452a5a8ade462779f14962
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-10-17 22:50:58 +0200

    Created movie-card.jsx (Ex- 3.3)

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,9 @@
+
+const MovieCard = ({movieData, onMovieClick}) =>{
+    // const {movieData} = props;
+    // const {onBookClick} = props;
+    return ( <div onClick={() =>{
+        onMovieClick(movieData);
+    }}>{movieData.Title}</div>)
+};
+export {MovieCard};
