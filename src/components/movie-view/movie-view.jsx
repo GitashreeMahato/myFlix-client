237bd21acc869068461d99bb74064e4f7cfbc195
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-10-19 15:33:51 +0200

    Updated movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,42 +1,42 @@
-const MovieView = ({movie, onBackClick})=>{
+const MovieView = ({movieData, onBackClick})=>{
     return (
         <>
         <div>
-            <img src={movie.imageURL} style={{height: "350px", width: "300px" }} />
+            <img src={movieData.imageURL} style={{height: "350px", width: "300px" }} />
         </div>
         <br />
         <div>
             <span>Title : </span>
-            <span>{movie.Title}</span>
+            <span>{movieData.Title}</span>
         </div>
         <div>
             <span>Description : </span>
-            <span>{movie.Description}</span>
+            <span>{movieData.Description}</span>
         </div>
         <div>
             <span>Genres : </span>
-            <span>{movie.Genres.Name}</span>
+            <span>{movieData.Genres.Name}</span>
         </div>
         <div>
             <span>Directors : </span>
-            <span>{movie.Directors.Name}</span>
+            <span>{movieData.Directors.Name}</span>
         </div>
         <div>
             <span>Actors : </span>
-                <span>{movie.Actors.Name }  </span>            
+                <span>{movieData.Actors }  </span>            
         </div>
         <div>
             <span> Release_date: </span>
-            <span>{movie.Release_date}</span>
+            <span>{movieData.Release_date}</span>
         </div>
         <div>
             <span>Rating : </span>
-            <span>{movie.Rating}</span>
+            <span>{movieData.Rating}</span>
         </div>
         <div>
             <span>Featured : </span>
-            <span>{movie.Featured}</span>
-        </div>  
+            <span>{movieData.Featured}</span>
+        </div>
         <br />
         <button onClick={onBackClick}>Back</button>
         </>
