c1ceae550e85f5f50a6d4ff332020fce08fdc96c
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-10-24 23:22:00 +0200

    Updated using react bootstrap (Ex- 3.6)

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,19 +1,35 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap"; 
 const MovieCard = ({movie, onMovieClick}) =>{
     
     return ( 
-    <div onClick={() =>{
-        onMovieClick(movie);
-    }}>{movie.Title}</div>)
-};
+//     <div onClick={() =>{
+//         onMovieClick(movie);
+//     }}>{movie.Title}</div>)
+// };
+
+<Card className="h-100">
+      <Card.Img variant="top" src={movie.imageURL}  />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Button onClick={() => onMovieClick(movie)} variant="link">
+          More details
+        </Button>
+      </Card.Body>
+    </Card>
+)}
+    
 // define all the props constraints for the BookCard
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-        ImageURL: PropTypes.string.isRequired,
+        imageURL: PropTypes.string.isRequired,
         Title: PropTypes.string.isRequired,
         // Directors: PropTypes.string.isRequired,
         // Genres: PropTypes.string.isRequired,
 
+    
+    }).isRequired,
     onMovieClick: PropTypes.func.isRequired
-    })};
+};
 export {MovieCard};
