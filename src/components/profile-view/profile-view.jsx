640d96024071746b83b7a67176f05e6ddcd78845
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2023-11-27 19:34:04 +0100

    Updated profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,37 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { Col, Container } from 'react-bootstrap';
 // import { MovieCard } from '../movie-card/movie-card';
 import { Button, Card } from 'react-bootstrap';
 import './profile-view.scss';
 import { FaUserEdit } from 'react-icons/fa';
 import Swal from 'sweetalert2';
+import { API_URL } from '../../config';
 
 export const ProfileView = ({ user, token, loggedOut }) => {
   //function to do a delete request and delete the users account
   const deleteAccount = () => {
-    fetch(`hhttps://user-movies-b3ba594615fa.herokuapp.com//users/${user.username}`, {
-      method: 'DELETE',
+    fetch(`${API_URL}/users/${user.username}`, {
+      method: "DELETE",
       headers: { Authorization: `Bearer ${token}` }
     })
       .then((response) => {
         if (response.ok) {
           //custom ui alert for a user deleting their account
           Swal.fire({
-            title: 'Are you sure you want to delete your account?',
+            title: "Are you sure you want to delete your account?",
             text: "You won't be able to revert this!",
-            icon: 'warning',
+            icon: "warning",
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
+            confirmButtonText: "Yes, delete it!"
           }).then((result) => {
             if (result.isConfirmed) {
               Swal.fire(
-                'Deleted!',
-                'Your account has been deleted.',
-                'success'
+                "Deleted!",
+                "Your account has been deleted.",
+                "success"
               );
-              window.location.replace('/login');
+              // window.location.replace("/login"  );
+              <Navigate to="/login" replace />
               loggedOut();
             }
           });
@@ -46,9 +48,12 @@ export const ProfileView = ({ user, token, loggedOut }) => {
     <>
       <Container id='profile-container'>
         <div className='mx-auto' id='box-profile-view'>
-          <div id='form-signup'>
+          <div id='form-signUp'>
             
             <Col className='mt-2'>
+            <Link to={`/users/updateUser`} className="user-edit">
+              <FaUserEdit size={50} />
+            </Link>
               <Card.Text className='font-style'>
                 Username: {user.username}
               </Card.Text>
@@ -63,14 +68,12 @@ export const ProfileView = ({ user, token, loggedOut }) => {
             </Col>
 
             
-            {/* <Col> */}
-            <Link to={`/users/updateUser`} className='user-edit'>
-              <FaUserEdit size={50} />
-            </Link>
-            <Link to={`/users/updateUser`} className='user-edit'>
+            {/* <Col>
+            
+            <Link to={`/users/updateUser`} className="user-edit">
               <span className='edit-user font-style'>Manage Profile</span>
             </Link>
-            {/* </Col> */}
+            </Col> */}
             <Button
               className='delete-btn font-style'
               variant='btn btn-danger'
@@ -129,7 +132,7 @@ export const ProfileView = ({ user, token, loggedOut }) => {
 // ==========================================
 
 
-{/* <Col className='mt-2'>
+/* <Col className='mt-2'>
               <Link
                 to={`/users/password-update`}
                 id='password-link'
@@ -144,4 +147,4 @@ export const ProfileView = ({ user, token, loggedOut }) => {
               >
                 <Card.Text className='font-style'>Update Password</Card.Text>
               </Link>
-            </Col> */}
+            </Col> */
