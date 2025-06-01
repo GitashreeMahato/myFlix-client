bd7e498447753cc4e6ac5dba0580bfad3014abd4
GITASHREE MAHATO <mahato.gitashree@gmail.com>
2025-06-01 12:48:06 +0200

    updated API URL to Render deployment

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.BASE_API_URL || "https://user-movies-b3ba594615fa.herokuapp.com";
+// export const API_URL = process.env.BASE_API_URL || "https://user-movies-b3ba594615fa.herokuapp.com";
+export const API_URL = process.env.BASE_API_URL || "https://myflix-api-u098.onrender.com";
